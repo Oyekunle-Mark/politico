6878f19e301ffd030c508bca04133fc08202e62e
Oyekunle-Mark <oyekunlemac@gmail.com>
2019-01-26 02:47:34 +0100

    Added the controller class to the controller file

---
+++ controllers/controller.js
@@ -0,0 +1,48 @@
+const parties = [];
+
+const offices = [];
+
+class Controller {
+  createParty(req, res) {
+    const party = {
+      id: parties.length + 1,
+      name: req.body.name,
+      hqAddress: req.body.hq,
+      logoUrl: req.body.logo
+    }
+
+    parties.push(party);
+    
+    res.status(200).send({
+      status: 200,
+      data: [{
+        "id": party.id,
+        "name": party.name
+      }]
+    })
+  }
+
+  getSpecificParty(req, res) {
+    if (parties.length === 0) {
+      res.status(404).send({
+        status: 404,
+        errror: "No parties added"
+      })
+    }
+    
+    const id = parseInt(req.params.id);
+    const party = parties[id - 1];
+
+    res.status(200).send({
+      status: 200,
+      data: [{
+        id,
+        name: party.name,
+        logo: party.logo
+      }]
+    })
+  }
+}
+
+const controller = new Controller();
+export default controller;
