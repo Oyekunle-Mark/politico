a15a74d4eba6ea599165da6dac358edb3a82ba8c
Oyekunle-Mark <oyekunlemac@gmail.com>
2019-01-27 02:37:21 +0100

    Added the deleteSpecificParty controller to the routes

--- routes/apiV1.js
+++ routes/apiV1.js
@@ -7,5 +7,6 @@ router.post('/parties', partyController.createParty);
 router.get('/parties/:id', partyController.getSpecificParty);
 router.get('/parties/', partyController.getAllParty);
 router.patch('/parties/:id/name', partyController.editSpecificParty);
+router.delete('/parties/:id', partyController.deleteSpecificParty);
 
 export default router;
