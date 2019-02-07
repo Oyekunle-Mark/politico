cb074af1673e8340163037861c03288ac7c5ce60
Oyekunle-Mark <oyekunlemac@gmail.com>
2019-02-07 03:14:14 +0100

    ch(jwt): Secure all endpoint with JWT - Secures all the app endpoint  with JWT [Delivers #163673633]

--- model/db/db.js
+++ model/db/db.js
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db = new Pool();
+const db = new Pool('postgres://taugumejxpteng:823f783c86572a3d20debaf1139a736c577e0cec04f53d9b1fc14903879fd8d8@ec2-174-129-224-157.compute-1.amazonaws.com:5432/d13pisalls339h');
 
 export default db;
