a28dfed3ab4fd1ec941f9b7cb4d229c7fd06c3cd
Oyekunle-Mark <oyekunlemac@gmail.com>
2019-02-16 01:57:39 +0100

    Enabled CORS preflight on patch route

--- index.js
+++ index.js
@@ -15,19 +15,19 @@ const app = express();
 // app.use(cors());
 // app.options('/parties/:id/name', cors());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-  next();
-  // // allow preflight
-  // if (req.method === 'OPTIONS') {
-  //   res.send(200);
-  // } else {
-  //   next();
-  // }
-});
+//   next();
+//   // // allow preflight
+//   // if (req.method === 'OPTIONS') {
+//   //   res.send(200);
+//   // } else {
+//   //   next();
+//   // }
+// });
 
 //  use the morgan logging  middleware
 app.use(morgan('short'));
