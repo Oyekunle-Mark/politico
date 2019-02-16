5f123c786d8a3d214e7a0af5de0439d03cfa51f0
Oyekunle-Mark <oyekunlemac@gmail.com>
2019-02-16 01:54:36 +0100

    Enabled CORS preflight on patch route

--- index.js
+++ index.js
@@ -20,12 +20,13 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-  // allow preflight
-  if (req.method === 'OPTIONS') {
-    res.send(200);
-  } else {
-    next();
-  }
+  next();
+  // // allow preflight
+  // if (req.method === 'OPTIONS') {
+  //   res.send(200);
+  // } else {
+  //   next();
+  // }
 });
 
 //  use the morgan logging  middleware
