a2f23915753ae40f6bf62d0751cc61b39f9ef690
Oyekunle-Mark <oyekunlemac@gmail.com>
2019-02-21 12:45:55 +0100

    ft(get-all-party) Consumes the get all party api - gets all the parties from the database - renders the parties [Delivers #164086393]

--- js/admin.js
+++ js/admin.js
@@ -55,22 +55,59 @@ const createOffice = e => {
       name: officeType
     })
   })
-  .then(response => response.json())
-  .then(data => {
-    if (data.status === 201) {
-      alertMessage.textContent = "Office created.";
-      alert.classList.remove("hidden");
-      setTimeout(() => {
-        alert.classList.add("hidden");
-      }, 2000);
-    } else if (data.status === 500) {
-      alertMessage.textContent = "Office name already exist."
-      alert.classList.remove("hidden");
-      setTimeout(() => {
-        alert.classList.add("hidden");
-      }, 2000);
-    }
-  });
+    .then(response => response.json())
+    .then(data => {
+      if (data.status === 201) {
+        alertMessage.textContent = "Office created.";
+        alert.classList.remove("hidden");
+        setTimeout(() => {
+          alert.classList.add("hidden");
+        }, 2000);
+      } else if (data.status === 500) {
+        alertMessage.textContent = "Office name already exist."
+        alert.classList.remove("hidden");
+        setTimeout(() => {
+          alert.classList.add("hidden");
+        }, 2000);
+      }
+    });
 };
 
 document.querySelector(".createOfficeForm").addEventListener('submit', createOffice);
+
+const getAllParty = () => {
+  const table = document.querySelector('table');
+
+  fetch('https://politiko.herokuapp.com/api/v1/parties/', {
+    headers: {
+      'x-access-token': localStorage.token
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      table.innerHTML = '<tr><th>ID</th><th>Party Name</th><th>Logo</th><th></th><th></th></tr>';
+
+
+      data.data.forEach(party => {
+        const row = document.createElement('tr');
+        const partyId = document.createElement('td');
+        const partyName = document.createElement('td');
+        const partyLogo = document.createElement('td');
+        const editButton = document.createElement('td');
+        const deleteButton = document.createElement('td');
+        partyId.innerHTML = party.id;
+        partyName.innerHTML = party.name;
+        partyLogo.innerHTML = `<img src=${party.logourl} alt="party_logo" width="50px" height="50px">`;
+        editButton.innerHTML = `<button onclick=editParty(${party.id})>Edit</button>`;
+        deleteButton.innerHTML = `<button onclick=deleteParty(${party.id})>Delete</button>`
+        row.appendChild(partyId);
+        row.appendChild(partyName);
+        row.appendChild(partyLogo);
+        row.appendChild(editButton);
+        row.appendChild(deleteButton);
+        table.appendChild(row);
+      });
+    });
+}
+
+getAllParty();
