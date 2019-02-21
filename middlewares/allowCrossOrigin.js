6e1455f317fd23a5b4a8c5d2bdbda38fd6f2dae9
Oyekunle-Mark <oyekunlemac@gmail.com>
2019-02-21 01:13:43 +0100

    Added the x-access-token to CORS headers

--- middlewares/allowCrossOrigin.js
+++ middlewares/allowCrossOrigin.js
@@ -1,6 +1,6 @@
 const allowCrossOrigin = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, PATCH, OPTIONS');
   next();
 };
