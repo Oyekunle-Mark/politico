5b61d98b817041cbb0141188151cca893f545f59
Oyekunle-Mark <oyekunlemac@gmail.com>
2019-02-21 02:33:44 +0100

    ft(create-office) Consume the create office API - Consume the API for office creations - Creates and saves offices to the database [Delivers #164085972]

--- js/admin.js
+++ js/admin.js
@@ -33,3 +33,44 @@ const createParty = e => {
 };
 
 document.querySelector(".createPartyForm").addEventListener('submit', createParty);
+
+const createOffice = e => {
+  e.preventDefault();
+
+  const form = document.querySelector(".createOfficeForm");
+
+  const fn = document.getElementById("officeName");
+  const officeName = fn.options[fn.selectedIndex].text;
+  const t = document.getElementById("officeType");
+  const officeType = t.options[t.selectedIndex].text;
+
+  fetch("https://politiko.herokuapp.com/api/v1/offices", {
+    method: "post",
+    headers: {
+      'Content-Type': 'application/json',
+      'x-access-token': localStorage.token
+    },
+    body: JSON.stringify({
+      type: officeName,
+      name: officeType
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.status === 201) {
+      alertMessage.textContent = "Office created.";
+      alert.classList.remove("hidden");
+      setTimeout(() => {
+        alert.classList.add("hidden");
+      }, 2000);
+    } else if (data.status === 500) {
+      alertMessage.textContent = "Office name already exist."
+      alert.classList.remove("hidden");
+      setTimeout(() => {
+        alert.classList.add("hidden");
+      }, 2000);
+    }
+  });
+};
+
+document.querySelector(".createOfficeForm").addEventListener('submit', createOffice);
