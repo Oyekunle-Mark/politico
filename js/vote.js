e4c1bc0cb57ff83dcd18b556f40ec459e9d154f4
Oyekunle-Mark <oyekunlemac@gmail.com>
2019-02-27 20:21:50 +0100

    ft(consume-vote-api) Consumed the vote API - Allows a user to vote  a candidate - Sorts the list according to office type [Delivers #164086567]

--- js/vote.js
+++ js/vote.js
@@ -1,6 +1,8 @@
 const candidateList = [];
 
 const selectOffice = document.querySelector('.voteOfficeOption');
+const table = document.querySelector('table');
+table.innerHTML = '<h3>Choose the office your wish to vote for</h3>';
 
 const getFilterOption = () => {
   fetch('https://politiko.herokuapp.com/api/v1/offices', {
@@ -16,14 +18,13 @@ const getFilterOption = () => {
         option.innerHTML = `${office.type}`;
         selectOffice.appendChild(option);
       })
-
     });
 };
 
 getFilterOption();
 
-const populateCandidateList = () => {
-  fetch('https://politiko.herokuapp.com/api/v1/candidates', {
+const populateCandidateList = async () => {
+  await fetch('https://politiko.herokuapp.com/api/v1/candidates', {
     headers: {
       'x-access-token': localStorage.token
     }
@@ -36,12 +37,12 @@ const populateCandidateList = () => {
         candidateList[i].id = candidate.id;
         candidateList[i].party = candidate.party;
         candidateList[i].office = candidate.office;
-        candidateList[i].candidate = candidate.candidate;
+        candidateList[i].user = candidate.candidate;
         i++;
       })
     });
 
-  fetch('https://politiko.herokuapp.com/api/v1/parties/', {
+  await fetch('https://politiko.herokuapp.com/api/v1/parties/', {
     headers: {
       'x-access-token': localStorage.token
     }
@@ -58,7 +59,7 @@ const populateCandidateList = () => {
       });
     });
 
-  fetch('https://politiko.herokuapp.com/api/v1/auth/users', {
+  await fetch('https://politiko.herokuapp.com/api/v1/auth/users', {
     headers: {
       'x-access-token': localStorage.token
     }
@@ -67,7 +68,7 @@ const populateCandidateList = () => {
     .then(data => {
       data.data.forEach(user => {
         candidateList.forEach(candidate => {
-          if (candidate.candidate === user.id) {
+          if (candidate.user === user.id) {
             candidate.name = `${user.firstname} ${user.lastname}`;
             candidate.passport = user.passporturl;
           }
@@ -75,7 +76,79 @@ const populateCandidateList = () => {
       });
     });
 
-    console.log(candidateList);
+  console.log(candidateList);
 }
 
 populateCandidateList();
+
+const createBallot = (officeId) => {
+  console.log('create ballot called with', officeId);
+  table.innerHTML = `<tr><th>ID</th><th>Candidate</th><th>Candidate's Photo</th><th>Party</th><th>Party Logo</th><th></th></tr>`;
+  candidateList.forEach(candidate => {
+    console.log('hello')
+    if (candidate.office == officeId) {
+      const row = document.createElement('tr');
+      const candidateId = document.createElement('td');
+      const candidateName = document.createElement('td');
+      const candidatePhoto = document.createElement('td');
+      const partyName = document.createElement('td');
+      const partyLogo = document.createElement('td');
+      const voteButton = document.createElement('td');
+      candidateId.innerHTML = candidate.id;
+      candidateName.innerHTML = candidate.name;
+      candidatePhoto.innerHTML = `<img src=${candidate.passport} alt="candidate_passport" width="50px" height="50px">`;
+      partyName.innerHTML = candidate.party;
+      partyLogo.innerHTML = `<img src=${candidate.logo} alt="party_logo" width="50px" height="50px">`;
+      voteButton.innerHTML = `<button class="voteButton" onclick=voteCandidate(${candidate.office},${candidate.id})>Vote</button>`;
+      row.appendChild(candidateId);
+      row.appendChild(candidateName);
+      row.appendChild(candidatePhoto);
+      row.appendChild(partyName);
+      row.appendChild(partyLogo);
+      row.appendChild(voteButton);
+      table.appendChild(row);
+    }
+  })
+  console.log('after rendering');
+}
+
+const voteCandidate = (idOffice, idCandidate) => {
+  console.log('vote candidate called')
+  const alertMessage = document.querySelector("section.adminAlert div");
+
+  fetch('https://politiko.herokuapp.com/api/v1/votes/', {
+    method: "post",
+    headers: {
+      'Content-Type': 'application/json',
+      'x-access-token': localStorage.token
+    },
+    body: JSON.stringify({
+      office: idOffice,
+      candidate: idCandidate
+    })
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data.status === 201) {
+        alertMessage.textContent = "Vote cast!";
+        alertMessage.classList.remove("hidden");
+        setTimeout(() => {
+          alertMessage.classList.add("hidden");
+        }, 2000);
+      } else {
+        alertMessage.textContent = "Your have voted for this office in this election already."
+        alertMessage.classList.remove("hidden");
+        setTimeout(() => {
+          alertMessage.classList.add("hidden");
+        }, 2000);
+      }
+    })
+}
+
+const officeOption = () => {
+  const officeId = selectOffice.options[selectOffice.selectedIndex].value;
+  console.log('officeOption called with ', officeId);
+  createBallot(officeId);
+}
+
+selectOffice.addEventListener('change', officeOption);
